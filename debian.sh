5e2103159dbfcc4c6db1d15d02faa3a11a0a9163
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-02-11 13:18:20 -0500

    Uncomment

--- debian.sh
+++ debian.sh
@@ -16,23 +16,23 @@ sudo apt-get -y install \
 sudo easy_install pip
 sudo easy_install ansible
 
-# installdir="/tmp/laptop-$RANDOM"
-# mkdir $installdir
-#
-# git clone https://github.com/mdzhang/laptop.git $installdir
-# if [ ! -d $installdir ]; then
-#     echo "failed to find laptop."
-#     echo "git cloned failed"
-#     exit 1
-# else
-#     cd $installdir
+installdir="/tmp/laptop-$RANDOM"
+mkdir $installdir
+
+git clone https://github.com/mdzhang/laptop.git $installdir
+if [ ! -d $installdir ]; then
+    echo "failed to find laptop."
+    echo "git cloned failed"
+    exit 1
+else
+    cd $installdir
     sudo ansible-playbook -i inventory playbook.yml --verbose
-# fi
-#
-# echo "cleaning up..."
-#
-# rm -Rfv /tmp/$installdir
-#
-# echo "and we are done! Enjoy!"
-#
-# exit 0
+fi
+
+echo "cleaning up..."
+
+rm -Rfv /tmp/$installdir
+
+echo "and we are done! Enjoy!"
+
+exit 0
